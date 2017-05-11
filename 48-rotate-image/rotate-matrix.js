2ba7bec4def53376c59f88dcd3ad7637e9fb87e2
joshgo <joshgo@users.noreply.github.com>
2017-05-11 06:39:37 -0400

    leetcode/48 - Convert tabs to spaces

--- 48-rotate-image/rotate-matrix.js
+++ 48-rotate-image/rotate-matrix.js
@@ -1,59 +1,59 @@
 function createMatrix(n, randomMax) {
-	var randomizer = () => 0;
-	if (randomMax > 0) 
-		randomizer = () => Math.floor(Math.random() * randomMax);
+    var randomizer = () => 0;
+    if (randomMax > 0) 
+        randomizer = () => Math.floor(Math.random() * randomMax);
 
-	var matrix = [];
+    var matrix = [];
 
-	for(var i = 0; i < n; i++) {
-		var row = Array.from({length: n}, randomizer);
-		matrix.push(row);
-	}
+    for(var i = 0; i < n; i++) {
+        var row = Array.from({length: n}, randomizer);
+        matrix.push(row);
+    }
 
-	return matrix;
+    return matrix;
 }
 
 function rotate(matrix) {
-	var n = matrix[0].length;
-	var matrix90 = createMatrix(n, 0);
+    var n = matrix[0].length;
+    var matrix90 = createMatrix(n, 0);
 
-	for(var i = 0; i < n; i++)
-		for(var j = 0; j < n; j++)
-			matrix90[j][n-i-1] = matrix[i][j];
+    for(var i = 0; i < n; i++)
+        for(var j = 0; j < n; j++)
+            matrix90[j][n-i-1] = matrix[i][j];
 
-	for (var i = 0; i < n; i++)
-		for (var j = 0; j < n; j++)
-			matrix[i][j] = matrix90[i][j];
+    for (var i = 0; i < n; i++)
+        for (var j = 0; j < n; j++)
+            matrix[i][j] = matrix90[i][j];
 }
 
 function print(matrix, id) {
-	var str = "";
-	var n = matrix[0].length;
-	for(var i = 0; i < n; i++) {
-		str += matrix[i].join() + "<br/>";
-	}
+    var str = "";
+    var n = matrix[0].length;
+    for(var i = 0; i < n; i++) {
+        str += matrix[i].join() + "<br/>";
+    }
 
-	var el = document.getElementById(id);
-	el.innerHTML = str;
+    var el = document.getElementById(id);
+    el.innerHTML = str;
 }
 
 function clear(id) {
-	var el = document.getElementById(id);
-	el.innerHTML = '';
+    var el = document.getElementById(id);
+    el.innerHTML = '';
 }
 
 var theMatrix;
 var theSize;
 
 function generateMatrix() {
-	theSize = Number(document.getElementById('size').value);
-	theMatrix = createMatrix(theSize, 10);
-	print(theMatrix, 'matrix0');
-	clear('matrix1');
+    theSize = Number(document.getElementById('size').value);
+    theMatrix = createMatrix(theSize, 10);
+    print(theMatrix, 'matrix0');
+    clear('matrix1');
 }
 
 function solveMatrix() {
-	rotate(theMatrix);
-	print(theMatrix, 'matrix1');
+    rotate(theMatrix);
+    print(theMatrix, 'matrix1');
 }
 
